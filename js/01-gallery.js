afcba0addabf9b958b98c340e095d06d09fd6156
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-31 14:49:12 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(createGalleryItemsMarkUp (galleryItems));
 
-function createGalleryItemsMarkUp (galleryItems) {
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkUp = createGalleryItemsMarkUp(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkUp);
+
+galleryContainer.addEventListener('click', onContainerClick);
+
+function createGalleryItemsMarkUp(galleryItems) {
  const markUp = galleryItems.map(({preview, original, description}) => {
  return `
  <div class="gallery__item">
@@ -20,7 +26,18 @@ function createGalleryItemsMarkUp (galleryItems) {
  return markUp.join('');
 }
 
+function onImageClick(event) {
 
+console.log(event.target);
+}
+
+const OriginalImageUrl = event.target.dataset.source;
+
+const instance = basicLightbox.create(`
+    <img src="${OriginalImageUrl}" />
+`)
+
+instance.show()
 
 
 
