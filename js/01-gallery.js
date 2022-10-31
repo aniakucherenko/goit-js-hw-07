a141d9dbf1c31d9a06664736ed808900b0a795aa
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-31 13:33:07 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(createGalleryItemsMarkUp (galleryItems));
+
+function createGalleryItemsMarkUp (galleryItems) {
+ const markUp = galleryItems.map(({preview, original, description}) => {
+ return `
+ <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img 
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"/>
+      </a>
+    </div>
+ `;
+
+ });
+ return markUp.join('');
+}
+
+
+
+
 
 console.log(galleryItems);
