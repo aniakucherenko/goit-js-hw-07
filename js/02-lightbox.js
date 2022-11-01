d74a15e123f9ce0712bc0a177f5af1fa1a31fd96
aniakucherenko <ania.kucherenko@gmail.com>
2022-11-01 14:23:19 +0200

    update part2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,7 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
 const galleryContainer = document.querySelector('.gallery');
+
 const itemsMarkUp = createGalleryItemsMarkUp(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkUp);
@@ -8,16 +9,20 @@ galleryContainer.insertAdjacentHTML('beforeend', itemsMarkUp);
 function createGalleryItemsMarkUp(items) {
     const markUp = items.map(({preview, original, description}) => {
     return `
+    <li>
     <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>
+</li>
     `;
     });
-   
     return markUp.join('');
    }
 
+   const lightbox = new SimpleLightbox('.gallery a', { 
+    animationSpeed:	"250",
+    captionsData: "alt",
 
-
+});
 
 console.log(galleryItems);
